d45e0e9be977f1387fd5af9e9167c2a777ad824b
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-04-09 20:18:21 +0100

    added 404 route

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const formBody = require("body/form")
 const anyBody = require("body/any")
 const {fetchImageMetadata, createUser } = require('./services')
 
-const server = https.createServer({
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-});
+// const server = https.createServer({
+//     key: fs.readFileSync('./key.pem'),
+//     cert: fs.readFileSync('./cert.pem')
+// });
+
+const server = http.createServer();
 
 server.on('request', (req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -22,20 +24,27 @@ server.on('request', (req, res) => {
         const metadata = fetchImageMetadata(id);
 
         console.log(req.headers);
-    };
+    } else if (req.method === 'POST' && parsedUrl.pathname ==='/user') {
+        jsonBody(req, res, (err, body) => {
 
-    jsonBody(req, res,(err, body) => {
+            if (err) {
+                console.log(err);
+            } else {
 
-        if (err) {
-            console.log(err);
-        } else {
+                createUser(body['username']);
+            }
+        })
+    
+    } else {
+        res.statusCode = 404;
+        res.setHeader('X-Powered-By', 'Node');
+        res.end();
+    }
 
-            createUser(body['username']);
-        }
-    })
-    res.end('This was a https server')
+    
+    
 });
 
-server.listen(443, () => {
+server.listen(8080, () => {
     console.log('server is running')
 });
