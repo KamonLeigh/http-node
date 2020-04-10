f66918a8b9c07531c2039c643debe1daa852dbb3
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-04-10 21:43:22 +0100

    added all method and tranformRequest

--- axios.js
+++ axios.js
@@ -18,3 +18,44 @@ axios({
  .catch((error) => {
      console.error(error);
  })
+
+ axios({
+         method: 'post',
+         url: 'http://localhost:8080/user',
+         data: {
+             username: 'leigh'
+         },
+         transformRequest: (data, headers) => {
+             const newData = {
+                 username: data.username + "!"
+             }
+
+             return JSON.stringify(newData);
+         }
+     })
+     .then((response) => {
+        //  response.data.pipe(fs.createWriteStream('google.html'))
+
+        console.log(response.data);
+     })
+     .catch((error) => {
+         console.error(error);
+     })
+
+     // You can use transformResponse 
+
+     const getMetadata = () => {
+         return axios.get('http://localhost:8080/metadata?id=1')
+     }
+
+     const getMetadataAgain = () => {
+         return axios.get('http://localhost:8080/metadata?id=1')
+     }
+
+     axios.all([
+         getMetadata(),
+         getMetadataAgain()
+     ]).then((responseArray) => {
+         console.log(responseArray[0].data.description);
+         console.log(responseArray[1].data.description);
+     })
